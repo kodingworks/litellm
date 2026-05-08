22d6e798653faae6a5004432553463f182e0e963
Ariaseta Setia Alam <07ariaseta@gmail.com>
2026-05-08 14:39:02 +0700

    Simplify get_response_cost_from_hidden_params function
    Removed unnecessary checks and simplified the retrieval of response cost from hidden parameters.

--- litellm/cost_calculator.py
+++ litellm/cost_calculator.py
@@ -1668,20 +1668,6 @@ def completion_cost(  # noqa: PLR0915
 def get_response_cost_from_hidden_params(
     hidden_params: Union[dict, BaseModel],
 ) -> Optional[float]:
-    if isinstance(hidden_params, BaseModel):
-        _hidden_params_dict = cast(BaseModel, hidden_params).model_dump()
-    else:
-        _hidden_params_dict = hidden_params
-
-    additional_headers = _hidden_params_dict.get("additional_headers", {})
-    if (
-        additional_headers
-        and "llm_provider-x-litellm-response-cost" in additional_headers
-    ):
-        response_cost = additional_headers["llm_provider-x-litellm-response-cost"]
-        if response_cost is None:
-            return None
-        return float(additional_headers["llm_provider-x-litellm-response-cost"])
     return None
 
 
